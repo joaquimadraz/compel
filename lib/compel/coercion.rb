872132bb4ccf0c016db704a41192bc7fc5b7b0a8
joaquimadraz <joaquim.adraz@gmail.com>
2015-12-22 14:15:42 +0000

    remove unnecessary module scope

--- lib/compel/coercion.rb
+++ lib/compel/coercion.rb
@@ -47,7 +47,7 @@ module Compel
         return (/(false|f|no|n|0)$/i === value.to_s ? false : (/(true|t|yes|y|1)$/i === value.to_s ? true : nil)) if type == TrueClass || type == FalseClass || type == Boolean
         return nil
       rescue
-        raise Compel::InvalidParameterError, "'#{value}' is not a valid #{type}"
+        raise InvalidParameterError, "'#{value}' is not a valid #{type}"
       end
     end
 
