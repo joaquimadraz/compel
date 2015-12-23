dfbd6ce23d810a1c806829a00b63afe812409f29
joaquimadraz <joaquim.adraz@gmail.com>
2015-12-23 15:09:03 +0000

    specs for in, within and range validations

--- spec/compel/validation_spec.rb
+++ spec/compel/validation_spec.rb
@@ -29,6 +29,45 @@ describe Compel::Validation do
 
     end
 
+    context 'in, within, range' do
+
+      def expect_be_in_within_range(range, value)
+        [:in, :within].each do |key|
+          errors = Compel::Validation.validate(value, { key => range })
+          yield errors
+        end
+      end
+
+      it 'should validate without errors' do
+        expect_be_in_within_range(['PT', 'UK'], 'PT') do |errors|
+          expect(errors.empty?).to eq(true)
+        end
+      end
+
+      it 'should validate with errors' do
+        expect_be_in_within_range(['PT', 'UK'], 'US') do |errors|
+          expect(errors).to eq(['must be within: ["PT", "UK"]'])
+        end
+      end
+
+      context 'range' do
+
+        it 'should validate without errors' do
+          errors = Compel::Validation.validate(2, range: (1..3))
+
+          expect(errors.empty?).to eq(true)
+        end
+
+        it 'should validate with errors' do
+          errors = Compel::Validation.validate(4, range: (1..3))
+
+          expect(errors).to eq(['must be within: 1..3'])
+        end
+
+      end
+
+    end
+
   end
 
 end
