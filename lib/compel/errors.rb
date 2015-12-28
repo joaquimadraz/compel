8c459226875c8e3327caa843b63ec4b41bba6855
joaquimadraz <joaquim.adraz@gmail.com>
2015-12-28 22:36:23 +0000

    refactoring Compel::Errors for better understading
    we can add:
    - strings (that will be transformed to array)
    - arrays (concat with existing ones for that key)
    - hashes (will merge with existing ones for that key)

--- lib/compel/errors.rb
+++ lib/compel/errors.rb
@@ -7,33 +7,29 @@ module Compel
     end
 
     def add(key, error)
-      if error.is_a?(Compel::Errors)
-        if error.empty?
-          return
-        end
+      if error.empty?
+        return
+      end
 
+      if error.is_a?(Compel::Errors) || error.is_a?(Hash)
         if @errors[key].nil?
           @errors[key] = {}
         end
 
         @errors[key].merge!(error.to_hash)
+      else
+        if @errors[key].nil?
+          @errors[key] = []
+        end
 
-        return
+        if !error.is_a?(Array)
+          error = [error]
+        end
+
+        @errors[key].concat(error)
       end
 
-      if !error.is_a?(Array)
-        error = [error]
-      end
-
-      if error.empty?
-        return
-      end
-
-      if @errors[key].nil?
-        @errors[key] = []
-      end
-
-      @errors[key].concat(error)
+      @errors
     end
 
     def length
