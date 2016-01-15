719ac94b52d9dc841971ff3aef90d4b8ff29626c
joaquimadraz <joaquim.adraz@gmail.com>
2016-01-15 15:55:37 +0000

    coerce CommonValue method with type
    - coerce #in and #range value items with correct type

--- lib/compel/builder/common_value.rb
+++ lib/compel/builder/common_value.rb
@@ -4,23 +4,66 @@ module Compel
     module CommonValue
 
       def in(value)
-        options[:in] = value
+        options[:in] = coerce_values_ary!(value, :in)
         self
       end
 
       def range(value)
-        options[:range] = value
+        options[:range] = coerce_values_ary!(value, :range)
         self
       end
 
       def min(value)
-        options[:min] = value
+        options[:min] = coerce_value!(value, :min)
         self
       end
 
       def max(value)
-        options[:max] = value
+        options[:max] = coerce_value!(value, :max)
         self
+      end
+
+      def coerce_values_ary!(value, method)
+        begin
+          fail if value.nil?
+
+          Coercion.coerce!(value, Coercion::Array)
+        rescue
+          raise_array_error(method)
+        end
+
+        unless value.all?{ |value| Coercion.coerce(value, self.type).valid? }
+          raise_array_values_error(method)
+        end
+
+        value
+      end
+
+      def coerce_value!(value, method)
+        begin
+          fail if value.nil?
+
+          Coercion.coerce!(value, self.type)
+        rescue
+          raise_value_error(method)
+        end
+
+        value
+      end
+
+      def raise_array_error(method)
+        raise TypeError, "#{self.class.human_name} ##{method} " \
+                         "value must an Array"
+      end
+
+      def raise_array_values_error(method)
+        raise TypeError, "All #{self.class.human_name} ##{method} values " \
+                         "must be a valid #{self.type.human_name}"
+      end
+
+      def raise_value_error(method)
+        raise TypeError, "#{self.class.human_name} ##{method} value " \
+                         "must be a valid #{self.type.human_name}"
       end
 
     end
