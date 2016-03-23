2ab6dddd71b5a4bd87c5e247617121c1f29a23ca
Joaquim Adráz <joaquim@probox.local>
2016-03-23 21:13:12 +0000

    remove warning

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ require 'simplecov'
 require 'codeclimate-test-reporter'
 
 SimpleCov.start do
-  formatter SimpleCov::Formatter::MultiFormatter[
+  formatter SimpleCov::Formatter::MultiFormatter.new [
     SimpleCov::Formatter::HTMLFormatter,
     CodeClimate::TestReporter::Formatter
   ]
